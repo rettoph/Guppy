5ffa2e329ee0fbd276756cbed92838a1d76d0123
Rettoph <turner.anthony.d@gmail.com>
2019-04-18 15:17:40 -0600

    Add clicked event to button

--- Guppy.UI/Elements/TextButton.cs
+++ Guppy.UI/Elements/TextButton.cs
@@ -9,9 +9,29 @@ namespace Guppy.UI.Elements
 {
     public class TextButton : TextElement
     {
+        private Boolean _mouseDown;
+        public event EventHandler<TextButton> OnClicked;
+
         public TextButton(string text, Unit x, Unit y, Unit width, Unit height, Style style = null) : base(text, x, y, width, height, style)
         {
             this.StateBlacklist = ElementState.Active;
+
+            this.OnStateChanged += this.HandleStateChanged;
+        }
+
+        private void HandleStateChanged(object sender, Element e)
+        {
+            if(this.State == ElementState.Pressed)
+                _mouseDown = true;
+            else if(this.State == ElementState.Hovered && _mouseDown)
+            {
+                _mouseDown = false;
+                this.OnClicked?.Invoke(this, this);
+            }
+            else
+            {
+                _mouseDown = false;
+            }
         }
     }
 }
