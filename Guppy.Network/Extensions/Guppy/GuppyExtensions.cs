ee5ad33960d632814014a0cae0e6d65d4cd2459c
Rettoph <turner.anthony.d@gmail.com>
2019-05-16 17:08:50 -0600

    Extract scoped group services

--- Guppy.Network/Extensions/Guppy/GuppyExtensions.cs
+++ Guppy.Network/Extensions/Guppy/GuppyExtensions.cs
@@ -10,15 +10,12 @@ namespace Guppy.Network.Extensions.Guppy
 {
     public static class GuppyExtensions
     {
-        public static void ConfigureNetwork<TPeer, TGroup>(this GuppyLoader guppy, Func<IServiceProvider, TPeer> peerFactory, NetworkSceneDriver networkSceneDriver)
+        public static void ConfigureNetwork<TPeer>(this GuppyLoader guppy, Func<IServiceProvider, TPeer> peerFactory, NetworkSceneDriver networkSceneDriver)
             where TPeer : Peer
-            where TGroup : Group
         {
             guppy.Services.AddScene<NetworkScene>();
             guppy.Services.AddSingleton<TPeer>(peerFactory);
             guppy.Services.AddSingleton<Peer>(GuppyExtensions.GetPeer<TPeer>);
-            guppy.Services.AddScoped<TGroup>(GuppyExtensions.GetGroup<TGroup>);
-            guppy.Services.AddScoped<Group>(GuppyExtensions.GetGroup<Group>);
             guppy.Services.AddSingleton<NetworkSceneDriver>(networkSceneDriver);
         }
 
@@ -26,12 +23,6 @@ namespace Guppy.Network.Extensions.Guppy
             where TPeer : Peer
         {
             return provider.GetRequiredService<TPeer>();
-        }
-
-        private static TGroup GetGroup<TGroup>(IServiceProvider provider)
-            where TGroup : Group
-        {
-            return provider.GetRequiredService<NetworkScene>().group as TGroup;
         }
     }
 }
