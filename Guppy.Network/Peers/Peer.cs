9e6b478253d3e972a891ecddfdda8b1743caf2e2
Rettoph <turner.anthony.d@gmail.com>
2019-08-05 15:44:23 -0600

    Begin network rewrite

---
+++ Guppy.Network/Peers/Peer.cs
@@ -0,0 +1,42 @@
+﻿using Guppy.Extensions.DependencyInjection;
+using Guppy.Implementations;
+using Lidgren.Network;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guppy.Network.Peers
+{
+    /// <summary>
+    /// The core peer class.
+    /// This defines peer unspecific
+    /// functionality. Client or server 
+    /// specific functions will be implemented
+    /// within the ClientPeer and ServerPeer
+    /// classes.
+    /// </summary>
+    public abstract class Peer : Reusable
+    {
+        #region Private Fields
+        private NetPeer _peer;
+        #endregion
+
+        #region Constructor
+        public Peer(NetPeer peer)
+        {
+
+        }
+        #endregion
+
+        #region Lifecycle Methods
+        protected override void Create(IServiceProvider provider)
+        {
+            base.Create(provider);
+
+            // Save internal scope values...
+            provider.SetConfigurationValue("peer", this);
+            provider.SetConfigurationValue("net-peer", _peer);
+        }
+        #endregion
+    }
+}
---
+++ Guppy.Network/ServiceLoaders/NetworkServiceLoader.cs
@@ -0,0 +1,43 @@
+﻿using Guppy.Attributes;
+using Guppy.Extensions.DependencyInjection;
+using Guppy.Interfaces;
+using Guppy.Network.Peers;
+using Lidgren.Network;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guppy.Network.ServiceLoaders
+{
+    /// <summary>
+    /// Default service loader that includes
+    /// core network services.
+    /// </summary>
+    [IsServiceLoader()]
+    public class NetworkServiceLoader : IServiceLoader
+    {
+        public void Boot(IServiceCollection services)
+        {
+            services.AddScoped<NetPeer>(p => p.GetConfigurationValue<NetPeer>("net-peer"));
+            services.AddScoped<Peer>(p => p.GetConfigurationValue<Peer>("peer"));
+        }
+
+        public void PreInitialize(IServiceProvider provider)
+        {
+            // throw new NotImplementedException();
+        }
+
+        public void Initialize(IServiceProvider provider)
+        {
+            // throw new NotImplementedException();
+        }
+
+        public void PostInitialize(IServiceProvider provider)
+        {
+            // throw new NotImplementedException();
+        }
+    }
+}
