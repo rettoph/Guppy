75de31215e55dc7b1310fde2c0411f381b043d80
rettoph <anthony@turner.ec>
2020-08-27 11:16:57 -0700

    Begin work on logging library

---
+++ src/Guppy.IO/ServiceLoaders/OutputServiceLoader.cs
@@ -0,0 +1,26 @@
+﻿using Guppy.Attributes;
+using Guppy.DependencyInjection;
+using Guppy.Extensions.DependencyInjection;
+using Guppy.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guppy.IO.ServiceLoaders
+{
+    [AutoLoad]
+    internal sealed class OutputServiceLoader : IServiceLoader
+    {
+        public void ConfigureServices(ServiceCollection services)
+        {
+            services.AddFactory<Logger>(p => new Logger());
+
+            services.AddSingleton<Logger>();
+        }
+
+        public void ConfigureProvider(ServiceProvider provider)
+        {
+            // throw new NotImplementedException();
+        }
+    }
+}
