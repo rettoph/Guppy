0654b8355b7f02d77675a3d8a23fa5b7f6b47b78
Rettoph <turner.anthony.d@gmail.com>
2020-11-11 10:18:50 -0700

    Remove unnecessary network related GuppyLoader extension Methods

--- src/Guppy.Network/ServiceLoaders/NetworkServiceLoader.cs
+++ src/Guppy.Network/ServiceLoaders/NetworkServiceLoader.cs
@@ -24,12 +24,28 @@ namespace Guppy.Network.ServiceLoaders
             services.AddFactory<ServiceList<User>>(p => new ServiceList<User>());
             services.AddFactory<ServiceList<Group>>(p => new ServiceList<Group>());
             services.AddFactory<Group>(p => p.GetService<Peer>().GroupFactory());
+            services.AddFactory<NetClient>(p => new NetClient(p.GetService<NetPeerConfiguration>()));
+            services.AddFactory<NetServer>(p => new NetServer(p.GetService<NetPeerConfiguration>()));
+            services.AddFactory<ClientPeer>(p => new ClientPeer());
+            services.AddFactory<ServerPeer>(p => new ServerPeer());
+            services.AddFactory<NetPeerConfiguration>(p => new NetPeerConfiguration("guppy"));
+            services.AddFactory<UserNetConnectionDictionary>(p => new UserNetConnectionDictionary());
 
             // Setup service scopes...
             services.AddTransient<User>();
             services.AddTransient<ServiceList<User>>();
             services.AddSingleton<ServiceList<Group>>();
             services.AddSingleton<Group>();
+            services.AddSingleton<NetClient>();
+            services.AddSingleton<NetServer>();
+            services.AddSingleton<ClientPeer>();
+            services.AddSingleton<ServerPeer>();
+            services.AddSingleton<NetPeerConfiguration>();
+            services.AddSingleton<UserNetConnectionDictionary>();
+
+            // Configure services...
+            services.AddSetup<NetPeer>((i, p, c) => p.AddLookupRecursive<NetPeer>(c), -20);
+            services.AddSetup<Peer>((i, p, c) => p.AddLookupRecursive<Peer>(c), -20);
         }
 
         public void ConfigureProvider(ServiceProvider provider)
