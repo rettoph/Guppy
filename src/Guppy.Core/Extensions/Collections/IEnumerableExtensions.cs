c98f46fb8430729a819e7401ae256708bd9b5206
Rettoph <anthony@turner.ec>
2020-12-01 01:03:43 -0700

    Add extre extenions methods

--- src/Guppy.Core/Extensions/Collections/IEnumerableExtensions.cs
+++ src/Guppy.Core/Extensions/Collections/IEnumerableExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Guppy.Extensions.Collections
@@ -12,6 +13,47 @@ namespace Guppy.Extensions.Collections
                 action(item);
 
             return source;
+        }
+
+        public static TSource MinBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> selector)
+        {
+            return source.MinBy(selector, null);
+        }
+
+        public static TSource MinBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> selector, IComparer<TKey> comparer)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (selector == null) throw new ArgumentNullException("selector");
+            comparer ??= Comparer<TKey>.Default;
+
+            using (var sourceIterator = source.GetEnumerator())
+            {
+                if (!sourceIterator.MoveNext())
+                {
+                    throw new InvalidOperationException("Sequence contains no elements");
+                }
+                var min = sourceIterator.Current;
+                var minKey = selector(min);
+                while (sourceIterator.MoveNext())
+                {
+                    var candidate = sourceIterator.Current;
+                    var candidateProjected = selector(candidate);
+                    if (comparer.Compare(candidateProjected, minKey) < 0)
+                    {
+                        min = candidate;
+                        minKey = candidateProjected;
+                    }
+                }
+                return min;
+            }
+        }
+
+        public static T TryAggregate<T>(this IEnumerable<T> source, Func<T, T, T> aggregater, T fallback = default)
+        {
+            if (source.Any())
+                return source.Aggregate(aggregater);
+            else
+                return fallback;
         }
     }
 }
--- src/Guppy/Extensions/Microsoft/Xna/Framework/Vector2Extensions.cs
+++ src/Guppy/Extensions/Microsoft/Xna/Framework/Vector2Extensions.cs
@@ -7,13 +7,25 @@ namespace Guppy.Extensions.Microsoft.Xna.Framework
 {
     public static class Vector2Extensions
     {
-        public static Single Angle(this Vector2 p1, Vector2 p2)
+        public static Single Angle(this Vector2 p2, Vector2 p1 = default)
             => MathHelper.WrapAngle((Single)Math.Atan2(p2.Y - p1.Y, p2.X - p1.X));
 
         public static Single Angle(this Vector2 vertex, Vector2 p1, Vector2 p2)
             => MathHelper.WrapAngle((Single)Math.Atan2(p2.Y - vertex.Y, p2.X - vertex.X) - (Single)Math.Atan2(p1.Y - vertex.Y, p1.X - vertex.X));
 
-        public static Vector2 Rotate(this Vector2 v, Single radians)
-            => Vector2.Transform(v, Matrix.CreateRotationZ(radians));
+        public static Vector2 Round(this Vector2 vector)
+            => new Vector2((Single)Math.Round(vector.X), (Single)Math.Round(vector.Y));
+
+        public static Vector2 Rotate(this Vector2 v, Single delta)
+            => v.RotateTo((Single)Math.Atan2(v.Y, v.X) + delta);
+
+        public static Vector2 RotateTo(this Vector2 v, Single target)
+        {
+            var l = v.Length();
+
+            return new Vector2(
+                x: (Single)Math.Cos(target) * l,
+                y: (Single)Math.Sin(target) * l);
+        }
     }
 }
