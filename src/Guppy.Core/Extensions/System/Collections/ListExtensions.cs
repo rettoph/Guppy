87c44e634d29e4510df5aeca3dc2cdcb5f31b246
rettoph <anthony@turner.ec>
2021-03-15 00:54:12 -0600

    Add some more event delegates & helper methods

---
+++ src/Guppy.Core/Extensions/System/Collections/ListExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Guppy.Extensions.System.Collections
+{
+    public static class ListExtensions
+    {
+        public static void TryAddRange<T>(this List<T> list, IEnumerable<T> collection)
+        {
+            if (collection?.Any() ?? false)
+                list.AddRange(collection);
+        }
+    }
+}
---
+++ src/Guppy/Events/OnGameTimeEventDelegate.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guppy.Events
+{
+    public delegate void OnGameTimeEventDelegate<TSender>(TSender sender, GameTime gameTime);
+    public delegate void OnGameTimeEventDelegate<TSender, TArgs>(TSender sender, GameTime gameTime, TArgs args);
+
+    public static class OnGameTimeEventDelegateExtensions
+    {
+        /// <summary>
+        /// Helper method to automatically invoke an event
+        /// call if the value has changed.
+        /// </summary>
+        /// <typeparam name="TSender"></typeparam>
+        /// <typeparam name="TArgs"></typeparam>
+        /// <param name="eventDelegate"></param>
+        /// <param name="valid">Whether or not the event should be invoked.</param>
+        /// <param name="sender">The sender instance.</param>
+        /// <param name="reference">A reference to be updated with the new value if valid.</param>
+        /// <param name="value">The value to set the reference if valid.</param>
+        public static Boolean InvokeIf<TSender, TArgs>(
+            this OnGameTimeEventDelegate<TSender, TArgs> eventDelegate,
+            Boolean valid,
+            TSender sender,
+            GameTime gameTime,
+            ref TArgs reference,
+            TArgs value)
+        {
+            if (valid)
+            {
+                reference = value;
+                eventDelegate?.Invoke(sender, gameTime, reference);
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
