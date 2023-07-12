9ad9ac883c83ba00d377e05fd522edd7c694f0c0
rettoph <anthony@turner.ec>
2023-07-12 15:19:38 -0600

    Create sequences to replace sorting

---
+++ src/Guppy.Common/Attributes/SequenceAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guppy.Common.Attributes
+{
+    public class SequenceAttribute<TSequence> : Attribute
+        where TSequence : Enum
+    {
+        public readonly TSequence Value;
+
+        public SequenceAttribute(TSequence value)
+        {
+            Value = value;
+        }
+    }
+}
---
+++ src/Guppy.Common/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Guppy.Common.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guppy.Common.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static T[] Sequence<T, TSequence>(this IEnumerable<T> items, TSequence defaultSequence)
+            where TSequence : Enum
+        {
+            return items.Select(x => (item: x, sequence: EnumerableExtensions.GetSequence<T, TSequence>(x, defaultSequence)))
+                .OrderBy(x => x.sequence)
+                .Select(x => x.item)
+                .ToArray();
+        }
+
+        private static TSequence GetSequence<T, TSequence>(T item, TSequence defaultSequence)
+            where TSequence : Enum
+        {
+            if (item is null)
+            {
+                return defaultSequence;
+
+            }
+
+            if (!item.GetType().HasCustomAttribute<SequenceAttribute<TSequence>>(true))
+            {
+                return defaultSequence;
+            }
+
+            return item.GetType().GetCustomAttribute<SequenceAttribute<TSequence>>()!.Value;
+        }
+    }
+}
