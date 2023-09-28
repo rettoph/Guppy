1135d41db2f9ce9eceb1582de7ad4600417c6193
rettoph <anthony@turner.ec>
2023-09-27 19:25:03 -0600

    Remove MonoGame

--- src/Guppy.MonoGame/Extensions/Microsoft/Xna/Framework/Vector2Extensions.cs
+++ src/Guppy.MonoGame/Extensions/Microsoft/Xna/Framework/Vector2Extensions.cs
@@ -7,31 +7,31 @@ namespace Microsoft.Xna.Framework
 {
     public static class Vector2Extensions
     {
-        public static Single Angle(this Vector2 p2, Vector2 p1)
-            => MathHelper.WrapAngle((Single)Math.Atan2(p2.Y - p1.Y, p2.X - p1.X));
+        public static float Angle(this Vector2 p2, Vector2 p1)
+            => MathHelper.WrapAngle(MathF.Atan2(p2.Y - p1.Y, p2.X - p1.X));
 
-        public static Single Angle(this Vector2 vertex, Vector2 p1, Vector2 p2)
-            => MathHelper.WrapAngle((Single)Math.Atan2(p2.Y - vertex.Y, p2.X - vertex.X) - (Single)Math.Atan2(p1.Y - vertex.Y, p1.X - vertex.X));
+        public static float Angle(this Vector2 vertex, Vector2 p1, Vector2 p2)
+            => MathHelper.WrapAngle(MathF.Atan2(p2.Y - vertex.Y, p2.X - vertex.X) - MathF.Atan2(p1.Y - vertex.Y, p1.X - vertex.X));
 
         public static Vector2 Round(this Vector2 vector)
-            => new Vector2((Single)Math.Round(vector.X), (Single)Math.Round(vector.Y));
+            => new Vector2(MathF.Round(vector.X), MathF.Round(vector.Y));
 
-        public static Vector2 Rotate(this Vector2 v, Single delta)
-            => v.RotateTo((Single)Math.Atan2(v.Y, v.X) + delta);
+        public static Vector2 Rotate(this Vector2 v, float delta)
+            => v.RotateTo(MathF.Atan2(v.Y, v.X) + delta);
 
-        public static Vector2 RotateTo(this Vector2 v, Single target)
+        public static Vector2 RotateTo(this Vector2 v, float target)
         {
             var l = v.Length();
 
             return new Vector2(
-                x: (Single)Math.Cos(target) * l,
-                y: (Single)Math.Sin(target) * l);
+                x: MathF.Cos(target) * l,
+                y: MathF.Sin(target) * l);
         }
 
-        public static String ToString(this Vector2 v, String format)
+        public static string ToString(this Vector2 v, string format)
             => $"({v.X.ToString(format)}, {v.Y.ToString(format)})";
 
-        public static Vector3 ToVector3(this Vector2 vector2, Single z = 0)
+        public static Vector3 ToVector3(this Vector2 vector2, float z = 0)
             => new Vector3(vector2.X, vector2.Y, z);
     }
 }
---
+++ src/Guppy.MonoGame/Utilities/TriangleHelper.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guppy.MonoGame.Helpers
+{
+    public struct Triangle
+    {
+        /// <summary>
+        /// Angle A
+        /// </summary>
+        public float A;
+
+        /// <summary>
+        /// Angle B
+        /// </summary>
+        public float B;
+
+        /// <summary>
+        /// Angle C
+        /// </summary>
+        public float C;
+
+        /// <summary>
+        /// Side a
+        /// </summary>
+        public float a;
+
+        /// <summary>
+        /// Side b
+        /// </summary>
+        public float b;
+
+        /// <summary>
+        /// Side c
+        /// </summary>
+        public float c;
+    }
+
+    public static class TriangleHelper
+    {
+        /// <summary>
+        /// Give the known triangle values and return
+        /// the full solved triangle
+        /// </summary>
+        /// <param name="A">Angle A</param>
+        /// <param name="B">Angle B</param>
+        /// <param name="C">Angle C</param>
+        /// <param name="a">Side a</param>
+        /// <param name="b">Side b</param>
+        /// <param name="c">Side c</param>
+        public static Triangle Solve(float? A = null, float? B = null, float? C = null, float? a = null, float? b = null, float? c = null)
+        {
+            var triangle = new Triangle()
+            {
+                A = A ?? 0,
+                B = B ?? 0,
+                C = C ?? 0,
+                a = a ?? 0,
+                b = b ?? 0,
+                c = c ?? 0
+            };
+
+            #region AAS
+            if (a != null && A != null && B != null)
+                TriangleHelper.AAS(A.Value, B.Value, a.Value, out triangle.C, out triangle.b, out triangle.c);
+            else if (a != null && A != null && C != null)
+                TriangleHelper.AAS(A.Value, C.Value, a.Value, out triangle.B, out triangle.c, out triangle.b);
+
+            else if (b != null && B != null && C != null)
+                TriangleHelper.AAS(B.Value, C.Value, b.Value, out triangle.A, out triangle.c, out triangle.a);
+            else if (b != null && B != null && A != null)
+                TriangleHelper.AAS(B.Value, A.Value, b.Value, out triangle.C, out triangle.a, out triangle.c);
+
+            else if (c != null && C != null && A != null)
+                TriangleHelper.AAS(C.Value, A.Value, c.Value, out triangle.B, out triangle.a, out triangle.b);
+            else if (c != null && C != null && B != null)
+                TriangleHelper.AAS(C.Value, B.Value, c.Value, out triangle.A, out triangle.b, out triangle.c);
+            #endregion
+
+            #region ASA
+            else if (a != null && C != null && B != null)
+                TriangleHelper.ASA(C.Value, B.Value, a.Value, out triangle.A, out triangle.b, out triangle.c);
+
+            else if (b != null && A != null && C != null)
+                TriangleHelper.ASA(A.Value, C.Value, b.Value, out triangle.B, out triangle.c, out triangle.a);
+
+            else if (c != null && B != null && A != null)
+                TriangleHelper.ASA(B.Value, A.Value, c.Value, out triangle.A, out triangle.a, out triangle.b);
+            #endregion
+
+            else
+                throw new Exception("Unsolvable triangle detected.");
+
+            return triangle;
+        }
+
+        public static void AAS(float a1, float a2, float s1, out float a3, out float s2, out float s3)
+        {
+            s2 = (float)((s1 * MathF.Sin(a2)) / MathF.Sin(a1));
+
+            a3 = MathHelper.Pi - a1 - a2;
+            s3 = ((s1 * MathF.Sin(a3)) / MathF.Sin(a1));
+        }
+
+        public static void ASA(float a1, float s1, float a2, out float a3, out float s2, out float s3)
+        {
+            a3 = MathHelper.Pi - a1 - a2;
+
+            s2 = ((s1 / MathF.Sin(a3)) * MathF.Sin(a1));
+            s3 = ((s1 * MathF.Sin(a3)) * MathF.Sin(a3));
+        }
+    }
+}
