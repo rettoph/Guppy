e7677662e9dc70ac5628b08bb037f9283a92c6f0
rettoph <anthony@turner.ec>
2024-05-26 15:05:54 -0600

    Add basic managed pointer

---
+++ src/core/Guppy.Core.Common/Utilities/ManagedPointer.cs
@@ -0,0 +1,43 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Guppy.Core.Common.Utilities
+{
+    public struct ManagedPointer<TValue>
+        where TValue : notnull
+    {
+        private readonly IntPtr _ptr;
+
+        public TValue Value
+        {
+            get
+            {
+                GCHandle handle = GCHandle.FromIntPtr(_ptr);
+                TValue value = (TValue?)handle.Target ?? throw new NotImplementedException();
+
+                return value;
+            }
+        }
+
+        public ManagedPointer()
+        {
+            throw new InvalidOperationException();
+        }
+
+        public ManagedPointer(TValue value)
+        {
+            GCHandle handle = GCHandle.Alloc(value, GCHandleType.Pinned);
+            _ptr = GCHandle.ToIntPtr(handle);
+        }
+
+        public void Free()
+        {
+            GCHandle handle = GCHandle.FromIntPtr(_ptr);
+            handle.Free();
+        }
+
+        public static implicit operator TValue(ManagedPointer<TValue> value)
+        {
+            return value.Value;
+        }
+    }
+}
